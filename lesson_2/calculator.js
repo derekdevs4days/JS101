82b501d5ec0479e03dd113aa10b4598b35332053
Derek Truong <derektruongdev@gmail.com>
2022-09-05 15:49:20 -0500

    ESlint

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -4,30 +4,58 @@
 //Perform the operation on the two numbers.
 //Print the result to the terminal.
 
-console.log('Welcome to Calculator!')
 
-const rlSync = require('readline-sync')
+const rlSync = require('readline-sync');
 
-let firstNum = Number(rlSync.question('What is your first number?\n'));
-let secondNum = Number(rlSync.question('What is your second number?\n'));
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-let operation = rlSync.question('What operation do you want to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide\n');
+function invalid(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
+
+prompt('Welcome to Calculator!');
+
+prompt('What is your first number?');
+let firstNum = rlSync.question();
+
+while (invalid(firstNum)) {
+  prompt('This is not a valid number!');
+  firstNum = rlSync.question();
+}
+
+prompt('What is your second number?');
+let secondNum = rlSync.question();
+while (invalid(secondNum)) {
+  prompt('This is not a valid number!');
+  secondNum = rlSync.question();
+}
+
+
+prompt('What operation do you want to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = rlSync.question();
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Enter a valid operation!');
+  operation = rlSync.question();
+}
 
 function doMath(a, b, op) {
-  let operation = op.toLowerCase();
 
-  if(op === '1') {
-    console.log(`${a} plus ${b} equals ${a + b}`)
-  }
-  if(op === '2') {
-    console.log(`${a} minus ${b} equals ${a - b}`)
-  }
-  if(op === '3') {
-    console.log(`${a} times ${b} equals ${a * b}`)
-  }
-  if(op === '4') {
-    console.log(`${a} divided by ${b} equals ${a / b}`)
+  switch (op) {
+    case '1':
+      console.log(`${a} plus ${b} equals ${Number(a) + Number(b)}`);
+      break;
+    case '2':
+      console.log(`${a} minus ${b} equals ${Number(a) - Number(b)}`);
+      break;
+    case '3':
+      console.log(`${a} times ${b} equals ${Number(a) * Number(b)}`);
+      break;
+    case '4':
+      console.log(`${a} divided by ${b} equals ${Number(a) / Number(b)}`);
+      break;
   }
 }
 
-doMath(firstNum, secondNum, operation)
+doMath(firstNum, secondNum, operation);
