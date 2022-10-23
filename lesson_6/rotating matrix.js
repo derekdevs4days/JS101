949fad7ef1ace492510dc5cd6e557d5b92f04974
Derek Truong <derektruongdev@gmail.com>
2022-10-23 17:35:32 -0500

    rotating matrix

---
+++ lesson_6/rotating matrix.js
@@ -0,0 +1,54 @@
+/* eslint-disable for-direction */
+/*
+Write a function that takes an arbitrary MxN matrix, rotates it clockwise by 90-degrees as described above, and returns the result as a new matrix. The function should not mutate the original matrix.
+
+let matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6],
+];
+
+let matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8],
+];
+
+let newMatrix1 = rotate90(matrix1);
+let newMatrix2 = rotate90(matrix2);
+let newMatrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))));
+
+console.log(newMatrix1);      // [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+console.log(newMatrix2);      // [[5, 3], [1, 7], [0, 4], [8, 2]]
+console.log(newMatrix3);      // `matrix2` --> [[3, 7, 4, 2], [5, 1, 0, 8]]
+ */
+
+function rotate90(matrix) {
+  let rotatedMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let current = [];
+    for (let j = matrix.length - 1; j >= 0; j--) {
+      current.push(matrix[j][i]);
+    }
+    rotatedMatrix.push(current);
+  }
+  return rotatedMatrix;
+}
+let matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6],
+];
+
+let matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8],
+];
+
+let newMatrix1 = rotate90(matrix1);
+let newMatrix2 = rotate90(matrix2);
+let newMatrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))));
+
+console.log(newMatrix1);      // [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+console.log(newMatrix2);      // [[5, 3], [1, 7], [0, 4], [8, 2]]
+console.log(newMatrix3);      // `matrix2` --> [[3, 7, 4, 2], [5, 1, 0, 8]]
+ 
